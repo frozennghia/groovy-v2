80651a2c2137260de390dea435680b38497daf61
Nghia Nguyen <n.nghia.hey@gmail.com>
2021-09-06 03:20:25 -0400

    Re-organized the files

---
+++ src/commands/groovy_commands.py
@@ -0,0 +1,92 @@
+import asyncio
+
+import discord
+from discord.ext import commands
+
+from youtube_wrap import yt_wrapper
+from discord.ext.commands import bot
+
+intents = discord.Intents().all()
+song_queue = asyncio.Queue()
+bot = commands.Bot(command_prefix="-", intents=intents)
+
+@bot.command(name='join', help='Tells the bot to join the voice channel')
+async def join(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
+        return
+    else:
+        channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+
+@bot.command(name='leave', help='To make the bot leave the voice channel')
+async def leave(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_connected():
+        await voice_client.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+
+@bot.command(name='play', help='Play song')
+async def queue_play(ctx, url):
+    song_queue.put_nowait([ctx, url])
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await ctx.send('Queued {}'.format(url))
+        return
+    while not song_queue.empty():
+        next_song = await song_queue.get()
+        await asyncio.create_task(play(next_song[0], next_song[1]))
+
+
+async def play(ctx, song_url):
+    voice_client = ctx.message.guild.voice_client
+    if ctx.message.author.voice and not voice_client.is_playing():
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+        async with ctx.typing():
+            filename = await yt_wrapper.YTDLSource.from_url(song_url, loop=bot.loop)
+            voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
+        await ctx.send('**Now playing:** {}'.format(filename))
+        await wait_to_finish(ctx)
+    else:
+        await ctx.send("Unknown Error")
+
+
+async def wait_to_finish(ctx):
+    voice_client = ctx.message.guild.voice_client
+    while voice_client.is_playing():
+        await asyncio.sleep(1)
+
+
+@bot.command(name='pause', help='This command pauses the song')
+async def pause(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await voice_client.pause()
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
+
+
+@bot.command(name='resume', help='Resumes the song')
+async def resume(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_paused():
+        await voice_client.resume()
+    else:
+        await ctx.send("The bot was not playing anything before this. Use play_song command")
+
+
+@bot.command(name='stop', help='Stops the song')
+async def stop(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await voice_client.stop()
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
+
+
+
+
