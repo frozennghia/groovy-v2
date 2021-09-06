2f99a874dbe14a84604c6361f719bc30e70353b8
Nghia Nguyen <n.nghia.hey@gmail.com>
2021-09-06 17:25:49 -0400

    Implemented the skip feature

--- src/commands/groovy_commands.py
+++ src/commands/groovy_commands.py
@@ -3,13 +3,15 @@ import asyncio
 import discord
 from discord.ext import commands
 
-from youtube_wrap import yt_wrapper
 from discord.ext.commands import bot
+
+from src.youtube_wrap import yt_wrapper
 
 intents = discord.Intents().all()
 song_queue = asyncio.Queue()
 bot = commands.Bot(command_prefix="-", intents=intents)
 bot.is_connected = False
+
 
 @bot.command(name='join', help='Tells the bot to join the voice channel')
 async def join(ctx):
@@ -24,21 +26,22 @@ async def join(ctx):
     bot.is_connected = True
 
 
-@bot.command(name='leave', help='To make the bot leave the voice channel')
+@bot.command(name='leave', help='Leave the channel')
 async def leave(ctx):
     voice_client = ctx.message.guild.voice_client
     if voice_client.is_connected():
         await voice_client.disconnect()
+        bot.is_connected = False
     else:
         await ctx.send("The bot is not connected to a voice channel.")
 
 
 @bot.command(name='play', help='Play song')
-async def queue_play(ctx, url):
+async def queue_play(ctx, url, is_skipped=False):
     await join(ctx)
-    song_queue.put_nowait([ctx, url])
-    voice_client = ctx.message.guild.voice_client
-    if voice_client.is_playing():
+    if not is_skipped:
+        song_queue.put_nowait([ctx, url])
+    if is_currently_playing(ctx):
         await ctx.send('Queued {}'.format(url))
         return
     while not song_queue.empty():
@@ -67,11 +70,17 @@ async def wait_to_finish(ctx):
         await asyncio.sleep(1)
 
 
+@bot.command(name='skip', help='Skips the current song that is playing')
+async def skip(ctx):
+    if is_currently_playing(ctx):
+        await stop(ctx)
+        asyncio.create_task(queue_play(ctx, "", True))
+
 @bot.command(name='pause', help='This command pauses the song')
 async def pause(ctx):
     voice_client = ctx.message.guild.voice_client
     if voice_client.is_playing():
-        await voice_client.pause()
+        voice_client.pause()
     else:
         await ctx.send("The bot is not playing anything at the moment.")
 
@@ -80,7 +89,7 @@ async def pause(ctx):
 async def resume(ctx):
     voice_client = ctx.message.guild.voice_client
     if voice_client.is_paused():
-        await voice_client.resume()
+        voice_client.resume()
     else:
         await ctx.send("The bot was not playing anything before this. Use play_song command")
 
@@ -89,6 +98,11 @@ async def resume(ctx):
 async def stop(ctx):
     voice_client = ctx.message.guild.voice_client
     if voice_client.is_playing():
-        await voice_client.stop()
+        voice_client.stop()
     else:
         await ctx.send("The bot is not playing anything at the moment.")
+
+
+def is_currently_playing(ctx):
+    voice_client = ctx.message.guild.voice_client
+    return voice_client.is_playing()
